b30fa344fe274e6b410ba501c1562d021cd91ded
Space9999 <sboddams@gmail.com>
2026-04-12 19:46:12 -0700

    Changes test

--- Test.py
+++ Test.py
@@ -4,8 +4,8 @@ import Loss_Functions
 import numpy as np
 import Layers
 
+# Just a quick test of the loss functions and the overall structure
 X = np.array([4, 5, 8, 9, 10, 20])
 Y = np.array([8, 10, 16, 18, 20, 40])
 network = NN.Base_Neural_Network(Optimizers.Simplified_SGD, Loss_Functions.mse, Loss_Functions.mse_grad, None)
-network.add(Layers.Dense(5, 6))
 print(network.train_batch(X, Y))
